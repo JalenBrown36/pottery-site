9858550f75f48c3f5e872ece4f3734d76615fc6d
Jalen Brown <jbrown36@umbc.edu>
2024-07-31 01:52:44 -0400

    Update Header.tsx and NavBar.tsx

--- app/ui/pages/Header.tsx
+++ app/ui/pages/Header.tsx
@@ -7,14 +7,14 @@ import { usePathname } from "next/navigation";
 
 export default function Header() {
   const pathname = usePathname();
-  const pageImage = clsx({
+  const pathImage = clsx({
     [style.galleryImage]: pathname === "/gallery",
     [style.partiesImage]: pathname === "/parties",
     [style.infoImage]: pathname === "/information",
   });
 
   return (
-    <header className={`${pageImage} py-16 bg-cover bg-no-repeat`}>
+    <header className={`${pathImage} py-16 bg-cover bg-no-repeat`}>
       <div className="w-2/3 mx-auto flex flex-col gap-8 justify-center items-center">
         <h1 className="w-full py-6 border-4 border-white text-center text-6xl font-bold text-white">
           TIPS & TECHNIQUES
--- app/ui/pages/Navbar.tsx
+++ app/ui/pages/Navbar.tsx
@@ -1,15 +1,17 @@
-import NavLink from "./NavLink"
+import NavLink from "./NavLink";
+import { usePathname } from "next/navigation";
 
 export default function NavBar() {
-    return (
-        <nav className="w-full">
-            <ul className="hidden text-center md:flex md:flex-row md:gap-4 md:justify-between md:text-sm lg:text-sm">
-                <NavLink url="/gallery" text="TIPS & TECHNIQUES" />
-                <NavLink url="/parties" text="PARTIES" />
-                <NavLink url="/information" text="FAQs" />
-                <NavLink url="/" text="GIFT CERTIFICATES" />
-            </ul>
-            
-        </nav>
-    )
+  const pathname = usePathname();
+  return (
+    <nav className="w-full">
+      <ul className="hidden text-center md:flex md:flex-row md:gap-4 md:justify-between md:text-sm lg:text-sm">
+        <NavLink url="/gallery" text="TIPS & TECHNIQUES" />
+        <NavLink url="/parties" text="PARTIES" />
+        <NavLink url="/information" text="FAQs" />
+        <NavLink url="/" text="GIFT CERTIFICATES" />
+        {pathname === "/" && <NavLink url="/" text="STUDIO HOURS" />}
+      </ul>
+    </nav>
+  );
 }
